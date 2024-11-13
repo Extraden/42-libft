ebc5fe50bff3d0a529492c62381c8f930ee13a65
Denis Semenov <dsemenov@z4r4p5.42lyon.fr>
2024-11-13 12:16:52 +0100

    fix ft_strrchr.c. Change the logic totally

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,33 +6,26 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:43:23 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/11 18:35:38 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/13 12:15:26 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stddef.h>
 
-char	*ft_strrchr(const char *s, int c)
+char    *ft_strrchr(const char *s, int c)
 {
-	size_t	i;
-	size_t	j;
 	char	*tmp;
 
-	tmp = (char *) s;
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (s[i] == (char) c)
-		{
-			j = i;
-		}
-		i++;
+	tmp = 0;
+	while (*s)
+	{  
+		if (*s == (char) c)
+			tmp = (char *) s;
+		s++;
 	}
-	if ((char) c == '\0')
-	{
-		return (tmp + i);
-	}
-	return (tmp + j);
+	if ((char) c == 0)
+		return ((char *) s);
+	return (tmp);
+	
 }
