d92b295344389325e8a7d9d01608b6c1e105c260
Denis Semenov <dsemenov@z3r8p5.42lyon.fr>
2024-11-14 19:41:30 +0100

    finish ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:41:39 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/14 18:21:35 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/14 19:41:06 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 size_t  ft_strlen(const char *s);
 
-size_t	first_index(char const *s1, const char *set)
+static size_t	first_index(char const *s1, const char *set)
 {
 	size_t	i;
 	size_t	j;
@@ -41,7 +41,7 @@ size_t	first_index(char const *s1, const char *set)
 }
 
 
-size_t	last_index(char const *s1, const char *set)
+static size_t	last_index(char const *s1, const char *set)
 {
 	size_t	i;
 	size_t	j;
@@ -65,45 +65,31 @@ size_t	last_index(char const *s1, const char *set)
 	return (i);
 }
 
+static char	*empty_str(void)
+{
+	char	*str;
+
+	str = malloc(1);
+	if (!str)
+		return (NULL);
+	str[0] = '\0';
+	return (str);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*res;
 	size_t	i;
-	size_t	j;
 	size_t	start;
 	size_t	end;
-	size_t	is_blank;
 
-	if (!*s1)
-	{
-		res = malloc(1);
-		res[0] = '\0';
-		return res;
-	}
-
-	is_blank = 1;
+	if (!s1)
+		return (NULL);
 	i = 0;
-	while (s1[i])
-	{
-		if (!((s1[i] >= 9 && s1[i] <= 13) || (s1[i] == ' ')))
-		{
-			is_blank = 0;
-			break;
-		}
-		i++;
-	}
-	if (is_blank)
-	{
-		res = malloc(1);
-		res[0] = '\0';
-		return (res);
-	}
-	i = 0;
-	j = 0;
 	start = first_index(s1, set);
 	end = last_index(s1, set);
-	
+	if (start > end || !*s1)
+		return (empty_str());
 	res = malloc(end - start + 2);
 	if (!res)
 		return (NULL);
