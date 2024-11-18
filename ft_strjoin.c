3b38ffea6b40f0ad323d642a1ff9ef6150a0fe44
Denis Semenov <dsemenov@z4r2p1.42lyon.fr>
2024-11-18 11:47:47 +0100

    fix ft_strjoin for norminette

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,15 +6,12 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:29:59 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/14 14:39:52 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/18 11:46:56 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-
-size_t  ft_strlen(const char *s);
-size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
