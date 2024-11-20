7f1c87aa39c7ed4e19325445d2e81a022af5386f
Denis Semenov <dsemenov@student.42lyon.fr>
2024-11-20 13:17:30 +0100

    finish ft_lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,13 +6,18 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 19:35:57 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/19 19:38:43 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:13:23 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stddef.h>
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
 }
