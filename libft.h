ac6ff2d7ba1fd9d1118cff3d450afb267e17a0ae
Denis Semenov <dsemenov@student.42lyon.fr>
2024-11-20 18:20:06 +0100

    add ft_lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:02:21 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/20 17:08:21 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/20 18:11:55 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ t_list  *ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
 
