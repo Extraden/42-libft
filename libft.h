87686b4d9e600cc10b22621abb1f16ef535e9a5e
Denis Semenov <dsemenov@student.42lyon.fr>
2024-11-20 14:05:04 +0100

    add ft_lstadd_back

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:02:21 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/20 12:57:53 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:19:54 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,7 @@ t_list  *ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
 t_list  *ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
 
