e4685ba04b10f497d6136b09e107d055649fb6eb
Denis Semenov <dsemenov@student.42lyon.fr>
2024-11-20 16:32:15 +0100

    add ft_lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:02:21 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/20 13:19:54 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/20 16:24:47 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
 t_list  *ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
 
