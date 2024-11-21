1db105e1a096132665947f415798bc38bf0048ad
Denis Semenov <dsemenov@student.42lyon.fr>
2024-11-21 16:21:48 +0100

    comment out main

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:10:05 by dsemenov          #+#    #+#             */
-/*   Updated: 2024/11/13 14:18:43 by dsemenov         ###   ########.fr       */
+/*   Updated: 2024/11/21 15:41:02 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,13 @@ int	ft_atoi(const char *nptr)
 
 /*
 #include <stdio.h>
-
-int	main(void)
+#include <unistd.h>
+#include <stdlib.h>
+int	main(int ac, char **av)
 {
-	char str[55] = "   55325 134";
-	printf("%d\n", ft_atoi(str));
+	(void) ac;
+	char str[55] = "   5525 134";
+	printf("%d\n", ft_atoi(av[1]));
+	printf("%d\n", atoi(av[1]));
 }
 */
